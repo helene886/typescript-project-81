867ff9af08098d94231dfaab1652bdf13d6547d3
Oseneva Elena <Elena.Oseneva@emerson.com>
2025-04-01 12:34:48 +0300

    Export default.

--- src/HexletCode.ts
+++ src/HexletCode.ts
@@ -1,8 +1,6 @@
 import { Form } from './Form'
-
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace HexletCode {
-  export function formFor(template: Record<string, unknown>, action: Record<string, unknown>, func: (param: Form) => void): Form {
+export default class HexletCode {
+  public static formFor(template: Record<string, unknown>, action: Record<string, unknown>, func: (param: Form) => void): Form {
     const form = new Form(template, action)
     func(form)
     return form
