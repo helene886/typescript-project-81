39d26d49a99b5e1fd1070be3ebc8b17bab9d5041
Oseneva Elena <Elena.Oseneva@emerson.com>
2025-04-01 11:15:57 +0300

    Export changed.

--- src/HexletCode.ts
+++ src/HexletCode.ts
@@ -1,9 +1,7 @@
 import { Form } from './Form'
 
-export class HexletCode {
-  public static formFor(template: Record<string, unknown>, action: Record<string, unknown>, func: (param: Form) => void): Form {
-    const form = new Form(template, action)
-    func(form)
-    return form
-  }
+export function formFor(template: Record<string, unknown>, action: Record<string, unknown>, func: (param: Form) => void): Form {
+  const form = new Form(template, action)
+  func(form)
+  return form
 }
