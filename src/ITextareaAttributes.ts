7b6e6f73797a50d3a48dbcca97e42f11ceaa1d6b
Oseneva Elena <Elena.Oseneva@emerson.com>
2025-04-07 11:12:45 +0300

    Required attributes updated.

--- src/ITextareaAttributes.ts
+++ src/ITextareaAttributes.ts
@@ -1,7 +1,7 @@
 export interface ITextareaAttributes extends Record<string, string | number | undefined> {
   rows?: number
   cols?: number
-  name: string
-  value: string
-  as: 'textarea'
+  name?: string
+  value?: string
+  as?: 'textarea'
 }
