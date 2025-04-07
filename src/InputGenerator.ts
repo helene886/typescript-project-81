752748cddfd8a02f9153938999cb9f20f65899d2
Oseneva Elena <Elena.Oseneva@emerson.com>
2025-04-07 11:46:53 +0300

    Minor update.

--- src/InputGenerator.ts
+++ src/InputGenerator.ts
@@ -5,17 +5,15 @@ import { TextareaTag } from './tags/TextareaTag'
 
 export class InputGenerator {
   public static generateInput(templateFieldName: string, templateFieldValue: string, attributes?: IInputAttributes | ITextareaAttributes): InputTag | TextareaTag {
-    if (attributes !== undefined) {
-      if (attributes.as !== undefined) {
-        attributes.value = templateFieldValue
-        attributes.name = templateFieldName
-        // following is to get expected attribute order for hexlet tests
-        const attrColsRowsOrder = attributes.cols || attributes.rows ? ['rows', 'cols'] : ['cols', 'rows']
-        const attributesOrder = attrColsRowsOrder.concat(['name']).concat(Object.keys(attributes).filter(e => !['cols', 'rows', 'name'].includes(e)))
-        const orderedAttributes: Record<string, string | number | undefined> = {}
-        attributesOrder.forEach(e => orderedAttributes[e] = attributes[e])
-        return new TextareaTag(orderedAttributes as ITextareaAttributes)
-      }
+    if (attributes?.as !== undefined) {
+      attributes.value = templateFieldValue
+      attributes.name = templateFieldName
+      // following is to get expected attribute order for hexlet tests
+      const attrColsRowsOrder = attributes.cols || attributes.rows ? ['rows', 'cols'] : ['cols', 'rows']
+      const attributesOrder = attrColsRowsOrder.concat(['name']).concat(Object.keys(attributes).filter(e => !['cols', 'rows', 'name'].includes(e)))
+      const orderedAttributes: Record<string, string | number | undefined> = {}
+      attributesOrder.forEach(e => orderedAttributes[e] = attributes[e])
+      return new TextareaTag(orderedAttributes as ITextareaAttributes)
     }
     // following is to get expected attribute order for hexlet tests
     let attrs: Record<string, string | number | undefined> = { name: templateFieldName }
